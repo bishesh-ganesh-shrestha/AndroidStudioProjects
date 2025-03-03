2112fedd8d61b5c74b9da7945448d0cea05b4c77
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-03-03 08:31:55 +0545

    toaster message and validation in Login Signup

--- loginSignupExample/app/src/main/java/edu/kathford/loginsignupexample/LoginActivity.java
+++ loginSignupExample/app/src/main/java/edu/kathford/loginsignupexample/LoginActivity.java
@@ -2,8 +2,11 @@ package edu.kathford.loginsignupexample;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,40 @@ public class LoginActivity extends AppCompatActivity {
         emailEditText = findViewById(R.id.emailEditText);
         pwEditText = findViewById(R.id.pwEditText);
         signInButton = findViewById(R.id.signInButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailEditText.getText().toString().trim();
+                String password = pwEditText.getText().toString().trim();
+
+//                emailValidation(email);
+//                pwValidation(password);
+
+                if(email.isEmpty()){
+                    emailEditText.setError("Email missing");
+                }else if(password.isEmpty()){
+                    pwEditText.setError("Password missing");
+                }else{
+                    Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                }
+
+                Log.d("Email", email);
+                Log.d("Password", password);
+
+            }
+        });
+    }
+
+    private void emailValidation(String email){
+        if(email.isEmpty()){
+            Toast.makeText(LoginActivity.this, "Email is missing.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void pwValidation(String password){
+        if(password.isEmpty()){
+            Toast.makeText(LoginActivity.this, "Password is missing.", Toast.LENGTH_LONG).show();
+        }
     }
 }

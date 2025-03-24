f420dad18d65a4bea02f2e5623d0e26d2b464ecf
stanleybishesh <unikbisheshshrestha12@gmail.com>
2025-03-24 08:07:21 +0545

    continue login signup example code

--- loginSignupExample/app/src/main/java/edu/kathford/loginsignupexample/SignUpActivity.java
+++ loginSignupExample/app/src/main/java/edu/kathford/loginsignupexample/SignUpActivity.java
@@ -1,4 +1,59 @@
 package edu.kathford.loginsignupexample;
 
-public class SignUpActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+    private EditText fullname,phone,password;
+    private RadioGroup genderRadioGroup;
+    private Spinner country;
+    private Button signupButton;
+    private String gender;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+        fullname = findViewById(R.id.fullNameEditText);
+        phone = findViewById(R.id.mobileNumberEditText);
+        password = findViewById(R.id.passwordEditText);
+        genderRadioGroup = findViewById(R.id.genderRadioGroup);
+        country = findViewById(R.id.countrySpinner);
+        signupButton = findViewById(R.id.signupButton);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fullNameEditText = fullname.getText().toString().trim();
+                String phoneEditText = phone.getText().toString().trim();
+            }
+        });
+
+        genderRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.maleRadioButton)
+                    gender = "Male";
+                else if(checkedId == R.id.femaleRadioButton)
+                    gender = "Female";
+                else
+                    gender = "Others";
+            }
+        });
+
+        country.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String[] country = new String[]{getString()
+            }
+        };
+    }
 }
